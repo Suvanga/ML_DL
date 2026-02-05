b59fdb19c0c31a2de2e4f5ce2337a567f792d606
Suvanga <83526481+Suvanga@users.noreply.github.com>
2026-02-05 11:42:45 -0500

    Fix typo in loss function variable name

--- engine.py
+++ engine.py
@@ -57,7 +57,7 @@ def train_step(model: torch.nn.Module,
           y_pred = model(X)
 
           #2 calculate the loss
-          loss = Loss_fn(y_pred, y)
+          loss = loss_fn(y_pred, y)
           train_loss += loss
           train_acc += accuracy_fn(y_true=y, y_pred=y_pred.argmax(dim=1))
 
